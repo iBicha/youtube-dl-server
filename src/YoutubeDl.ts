2592def7e16ac9b61632a1fd22b680c7f336b649
Brahim Hadriche <brahim.hadriche@unity3d.com>
2020-12-11 13:36:00 -0500

    Use double quotes for windows

--- src/YoutubeDl.ts
+++ src/YoutubeDl.ts
@@ -7,7 +7,7 @@ const bin = path.resolve('tools/bin/youtube-dl' +(isWin ? '.exe' : ''));
 
 export class YoutubeDl {
     public static async getVideoMetadata(url: string, options?: string) {
-        options = options ||  '-f \'best\'';
+        options = options ||  '-f \"best\"';
         const command = `${bin} ${options} --dump-json ${url}`;
         return await new Promise<any>((resolve, reject) => {
             exec(command, (error: ExecException | null, stdout: string, stderr: string) => {
