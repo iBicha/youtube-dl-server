9f056ce40c08a8cef74e48a7f7fc81fa46c40fae
Brahim Hadriche <brahim.hadriche@unity3d.com>
2020-12-11 13:01:39 -0500

    path fix

--- src/YoutubeDl.ts
+++ src/YoutubeDl.ts
@@ -1,13 +1,14 @@
-import {ExecException} from "child_process";
-
+import { ExecException } from "child_process";
 const { exec } = require("child_process");
+const path = require('path');
 
 const isWin = process.platform === "win32";
+const bin = path.resolve('tools/bin/youtube-dl' +(isWin ? '.exe' : ''));
 
 export class YoutubeDl {
     public static async getVideoMetadata(url: string, options?: string) {
         options = options ||  '-f \'best\'';
-        const command = `tools/bin/youtube-dl${isWin ? '.exe' : ''} ${options} --dump-json ${url}`;
+        const command = `${bin} ${options} --dump-json ${url}`;
         return await new Promise<any>((resolve, reject) => {
             exec(command, (error: ExecException | null, stdout: string, stderr: string) => {
                 if(error) {
