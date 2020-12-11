b84ce7e6a0de3fa50e181dd7429eb20803fc5dc8
Brahim Hadriche <brahim.hadriche@unity3d.com>
2020-12-10 21:49:08 -0500

    Revert "Add stream api"
    This reverts commit e627a2c34570ee8a3f1ab3c7d540e13023e7e6f0.

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,11 @@
 import express from 'express';
 const cors = require('cors');
 import {YoutubeDl} from "./YoutubeDl";
-import fetch from 'node-fetch';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors())
-
-app.get('/v1/video/stream', async (req, res) => {
-    try {
-        if (process.env.STREAMING_DISABLED){
-            res.status(403);
-            res.send('STREAMING_DISABLED');
-            return;
-        }
-        const url = req.query.url as string;
-        const options = req.query.options as string;
-        if(!url){
-            res.status(400);
-            res.send('Missing url');
-            return;
-        }
-        const metadata = await YoutubeDl.getVideoMetadata(url, options);
-        fetch(metadata.url).then(response => {
-            res.status(response.status);
-            response.headers.forEach((value, key) => {
-                res.set(key, value);
-            });
-            response.body.pipe(res);
-        })
-    } catch (e) {
-        console.error(e)
-        res.status(500);
-        res.send(e);
-    }
-});
 
 app.get('/v1/video', async (req, res) => {
     try {
