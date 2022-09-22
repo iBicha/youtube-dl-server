34535c03a24bd79a8989f7e8c914b0f72555d4b8
Brahim Hadriche <brahim.hadriche@gmail.com>
2022-09-22 12:51:15 -0400

    Add docker

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:16
+
+WORKDIR /usr/src/app
+
+COPY . .
+
+RUN npm install
+
+EXPOSE 8080
+CMD [ "npm", "start" ]
--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const compression = require('compression')
 import {YoutubeDl} from "./YoutubeDl";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(compression())
 app.use(cors())
